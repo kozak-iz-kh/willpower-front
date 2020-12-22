232f6ef04fe4d6945206a1d4ef3774af11aff36c
Igor <tsarenko.igor1@gmail.com>
2020-12-22 12:12:51 +0200

    Code style environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from "react";
 
-const App: FC = () => <div>"Hi"</div>;
+const App: FC = () => (
+  <div>Hi lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+);
 
 export default App;
